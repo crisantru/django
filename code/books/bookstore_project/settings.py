b4be4eeecf2f45a7c0c11b077713b533230781ad
Cristian Alberto Sánchez Trujillo <crisantruicode@gmail.com>
2020-04-09 14:03:58 -0500

    set environment variables

--- code/books/bookstore_project/settings.py
+++ code/books/bookstore_project/settings.py
@@ -8,10 +8,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '687n(kub$0ax+5h2s^83l0=s7m1%e=)*2!(4(ztg1$xm6##wz_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
